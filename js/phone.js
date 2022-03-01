41f8b0ba7130ca07933863a7d1e9a5541f03aaab
ahmednayeeme <ahmednaim353@gmail.com>
2022-03-01 23:19:30 +0600

    image-load

--- js/phone.js
+++ js/phone.js
@@ -15,20 +15,47 @@ const searchMobile = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(data => {
-        console.log(data);
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = ` 
-           <div class="card">
-           <img class="w-75" src="${data.image}" class="card-img-top" alt="...">
+           <div class="card w-75 h-100 p-2 mx-auto rounded">
+           <img src="${data.image}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${data.phone_name}</h5>
             <h6 class="card-text">${data.brand}</h6>
-            <p class="card-text">${data.slug}</p>
-            <a href="#" class="btn btn-outline-info">Details</a>
+            <button onclick="mobileDetail('${data.slug}')"
+            <a = href="#" class="btn btn-outline-info">Details</a>
+            </button>
+
         </div>
     </div>
          `;
         searchResult.appendChild(div);
     })
+}
+
+const mobileDetail = mobileId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMobileDetail(data.data))
+
+
+}
+
+const displayMobileDetail = data => {
+    console.log(data);
+    const mobileDetails = document.getElementById('mobile-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text">${data.brand}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>`
+
+    mobileDetails.appendChild(div)
 }
